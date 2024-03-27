daf16d235c2c92278eec7b1ec940f7960a11088e
RandimaWickramasinghe <cobsccomp222p-018@student.nibm.lk>
2024-03-27 09:54:12 +0530

    bhbb

--- eShop/SwiftUIView.swift
+++ eShop/SwiftUIView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct SwiftUIView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.title3)
+            .bold().foregroundStyle(.blue)
+            .padding()
+            
     }
 }
 
