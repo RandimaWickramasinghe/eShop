566b8cd79cc1dd6961b531555f5045c97dde1f65
RandimaWickramasinghe <cobsccomp222p-018@student.nibm.lk>
2024-03-27 09:42:37 +0530

    Initial Commit

---
+++ eShop/eShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  eShopApp.swift
+//  eShop
+//
+//  Created by Semini Wickramasinghe on 2024-03-27.
+//
+
+import SwiftUI
+
+@main
+struct eShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
