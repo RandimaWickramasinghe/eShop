788d7e0ab5c024e1f6e97fbb7f42a209ca98456f
RandimaWickramasinghe <cobsccomp222p-018@student.nibm.lk>
2024-03-27 09:51:48 +0530

    secobd update

---
+++ eShop/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  eShop
+//
+//  Created by Semini Wickramasinghe on 2024-03-27.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ eShop/view.swift
@@ -0,0 +1,8 @@
+//
+//  view.swift
+//  eShop
+//
+//  Created by Semini Wickramasinghe on 2024-03-27.
+//
+
+import Foundation
